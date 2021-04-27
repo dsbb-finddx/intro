633f82b8c528e5ae870260730c29269fe914178a
angelicambg <38508957+angelicambg@users.noreply.github.com>
2021-04-27 23:14:27 +0200

    changing R script
    Create csv file based on corrected columns

--- load_test.R
+++ load_test.R
@@ -7,9 +7,11 @@ tests_data <- readr::read_csv(
 today <- format(Sys.time(), "%Y-%m-%d")
 
 tests_data_today <- tests_data %>%
-
   filter(date == today) %>%
-
   select(country, tests_cumulative, pcr_tests_cum, rapid_test_cum)
+
+tests_corrected_today <- tests_data %>%
+  filter(date == today) %>%
+  select(pcr_tests_cum_corrected, rapid_test_cum_corrected)
 
 readr::write_csv(tests_data_today, "data/tests_today.csv")
